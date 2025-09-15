9547abfafdf5f4972f46553e15435d3eb32a542d
saprril <deafb463@gmail.com>
2025-09-15 09:01:18 +0700

    Initial Commit

---
+++ LogicExercise2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System.Text;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Starts of the program");
+    }
+}

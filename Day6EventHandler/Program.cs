be6d37aeed9093b84ceb3175c3a1704815019ff0
saprril <deafb463@gmail.com>
2025-09-16 10:04:11 +0700

    Initial Commit

---
+++ Day6EventHandler/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day6EventHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starts of program");
+        }
+    }
+}

d33b151a2b40620d81df53adf3c8b6f3297e32cb
saprril <deafb463@gmail.com>
2025-09-29 11:09:25 +0700

    Finished modulo processor

--- LogicExerciseFinal/FooBar.cs
+++ LogicExerciseFinal/FooBar.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using System.Data;
+using System.Text;
 
 namespace LogicExerciseFinal
 {
@@ -7,8 +9,39 @@ namespace LogicExerciseFinal
         public Dictionary<int, string> RuleSetDict { get; set; }
         public FooBar(int integerX, string stringX)
         {
-            Dictionary<int, string> RuleSetDict = new Dictionary<int, string>();
+            RuleSetDict = new Dictionary<int, string>();
             RuleSetDict[integerX] = stringX;
+        }
+
+        Func<int, string> FooBarProcessor = (val) =>
+        {
+            StringBuilder decisionSb = new StringBuilder();
+            return decisionSb.ToString();
+        };
+
+        public BitArray ModuloProcessor(int value)
+        {
+            BitArray divisibility = new BitArray(RuleSetDict.Count);
+            List<int> divisorList = RuleSetDict.Keys.OrderBy(k => k).ToList();
+
+            for (int i = 0; i < divisorList.Count; i++)
+            {
+                if (value % divisorList[i] == 0)
+                {
+                    foreach (var b in divisorList)
+                    {
+                        if (divisorList[i] == b) continue; // skip self
+                        if (divisorList[i] % b == 0)
+                        {
+                            divisibility[divisorList.IndexOf(b)] = false;
+                        }
+                    }
+                    divisibility[i] = true;
+                }
+            }
+            Console.WriteLine(string.Join("", divisibility.Cast<bool>().Select(b => b ? "1" : "0")));
+
+            return divisibility;
         }
 
         public void AddRules(int integerX, string stringX)
@@ -21,15 +54,17 @@ namespace LogicExerciseFinal
 
         public void RemoveRules(int integerX)
         {
-            if (!RuleSetDict.Keys.Contains(integerX))
+            if (RuleSetDict.Keys.Contains(integerX))
             {
                 RuleSetDict.Remove(integerX);
             }
         }
 
+
         public void PrintNumber(int number)
         {
             int[] numberArray = Enumerable.Range(1, number).ToArray();
+            string[] stringArray = numberArray.Select(FooBarProcessor).ToArray();
             Console.WriteLine(string.Join(", ", numberArray));
         }
     }
--- LogicExerciseFinal/Program.cs
+++ LogicExerciseFinal/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LogicExerciseFinal
+﻿using System.Collections;
+
+namespace LogicExerciseFinal
 {
     class Program
     {
@@ -6,9 +8,30 @@
         {
             // Console.WriteLine("Hello");
 
+
             FooBar fbController = new FooBar(3, "Foo");
 
             fbController.PrintNumber(10);
+            fbController.AddRules(6, "bar");
+            fbController.AddRules(5, "jazz");
+            BitArray sampleBA = fbController.ModuloProcessor(30);
+
+        }
+
+        public int GetInput()
+        {
+            Console.WriteLine("Give me some number: ");
+
+            string userInputtedNumber = Console.ReadLine() ?? "1";
+            int userInputtedInteger = 0;
+            if (Int32.TryParse(userInputtedNumber, out userInputtedInteger))
+            {
+                return userInputtedInteger;
+            }
+            else
+            {
+                return -1;
+            }
         }
     }
 }
